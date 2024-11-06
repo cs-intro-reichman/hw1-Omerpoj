2d3c2798a079e4495df1d44384786c892b18f50e
עומר פוזזינסקי <omer_pojejinsky@wmrs-MBP.lan>
2024-11-06 21:11:24 +0200

    TimeFormat

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,14 +3,13 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		boolean pm = hours>=12;
 		String pmOrAmStr="AM";
 		if (hours<10) {
-			hours = args[0].charAt(1);
+			hours = hours%10;
 		}
-		if (pm==true) {
-			hours=hours-12;
+		if (hours>=12) {
 			pmOrAmStr = "PM";
+			hours = hours-12;
 		}
         System.out.println(hours+":"+minutes+" "+pmOrAmStr);
 	}
