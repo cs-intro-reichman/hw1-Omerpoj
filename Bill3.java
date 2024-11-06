38e6b2eaa8d4480405b46e4ec88fd1a78ff6b078
עומר פוזזינסקי <omer_pojejinsky@wmrs-MBP.lan>
2024-11-06 22:36:26 +0200

    completed

--- Bill3.java
+++ Bill3.java
@@ -5,6 +5,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int value =Integer.parseInt(args[3]);
-		System.out.println("Dear "+name1+" "+name2+" "+name3+" pay "+(value/3+1)+ " each");
+		System.out.println("Dear "+name1+" "+name2+" "+name3+
+		                   " pay "+(value/3+1)+ " each");
 	}
 }
