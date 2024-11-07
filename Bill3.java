66f24ddf01419b963f922741bbf4054b595056e8
עומר פוזזינסקי <omer_pojejinsky@wmrs-MacBook-Pro.local>
2024-11-07 09:05:53 +0200

    and

--- Bill3.java
+++ Bill3.java
@@ -5,7 +5,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int value =Integer.parseInt(args[3]);
-		System.out.println("Dear "+name3+", "+name2+", "+name1+
+		System.out.println("Dear "+name3+", "+name2+", and"+name1+
 		                   ": pay "+(Math.ceil((double)value/3))+ " Shekels each");
 	}
 }
